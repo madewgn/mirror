fbc2c57cd065b671303dc250bea281a537f7a873
madewgn <madewg.n2@gmail.com>
2023-06-07 03:01:31 +0000

    x

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 apt update -y
-apt install docker.io -y
+apt install docker.io unzip wget git -y
+
+cd ~
+wget https://github.com/madewgn/mirror/raw/master/bot.zip
+
+clear
+echo "install berhasil"
+
+
 
 
